3b13421dd33aa365f21a236bcf42225d712101e2
facueslar <facundoeslar@gmail.com>
2024-10-31 09:56:09 -0300

    Cmabio backround

--- client/ProyectoMetaFundr- Tiferes/Project.js
+++ client/ProyectoMetaFundr- Tiferes/Project.js
@@ -1,16 +1,15 @@
- // Obtener el ID de la publicación desde la URL
  const urlParams = new URLSearchParams(window.location.search);
  const projectId = urlParams.get('id');
 
- // Hacer fetch de los detalles de la publicación usando el ID
  fetch(`http://localhost:8000/api/posts/${projectId}`)
      .then((response) => response.json())
      .then((publicacion) => {
          document.getElementById('project-title').textContent = publicacion.title;
          document.getElementById('project-description').textContent = publicacion.description;
-         document.getElementById('project-author').textContent = publicacion.author.username; // Si "author" es un nombre
+         document.getElementById('project-author').textContent = publicacion.author.username; 
          document.getElementById('project-thumbnail').src = publicacion.thumbnail;
-         document.getElementById('Foto.project').src = publicacion.thumbnail;
+         document.querySelector('.left-panel').style.backgroundImage = `url(${publicacion.thumbnail})`;
+
 
      })
      .catch((error) => {
