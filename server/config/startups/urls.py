3121e226f2da093404abafafd66d9452e2fae383
tobimjc <77305362+TobyMJC@users.noreply.github.com>
2024-10-31 14:17:39 -0300

    publicación y respuesta de comentarios posts

--- server/config/startups/urls.py
+++ server/config/startups/urls.py
@@ -3,10 +3,11 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-from .serializers import PostViewSet
+from .serializers import PostViewSet, PostCommentViewSet
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
+router.register(r'comments', PostCommentViewSet)
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
