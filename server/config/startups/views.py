2195a8af6734c4f74ae39bf61e507ec9d8af7bd4
tobimmj <166714447+tobimmj@users.noreply.github.com>
2024-11-04 16:48:50 -0300

    arreglos, fetch de todos los usuarios y modificacion de permisos al borrar posts

--- server/config/startups/views.py
+++ server/config/startups/views.py
@@ -3,10 +3,13 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
 from rest_framework import generics
-from .serializers import MetaUserSerializer
+from .models import MetaUser, Post, PostComment
+from .serializers import MetaUserSerializer, PostSerializer, PostCommentSerializer
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
-
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.exceptions import NotAuthenticated, PermissionDenied
 
 # Create your views here.
 age_limit = date.today() - relativedelta(years=18)
@@ -30,8 +33,59 @@ class RegisterView(generics.GenericAPIView):
         return Response({
             "access_token": str(AccessToken.for_user(user)),
             "refresh_token": str(RefreshToken.for_user(user))
-        })
+        }, status=status.HTTP_201_CREATED)
 
-@csrf_exempt
-def login(req):
-    pass
+#View sets
+
+class MetaUserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = MetaUser.objects.all()
+    serializer_class = MetaUserSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+    def perform_create(self, serializer):
+        if self.request.user.is_authenticated:
+            serializer.save(author=self.request.user)
+        else:
+            raise NotAuthenticated("You are not authenticated")
+
+    def destroy(self, request, *args, **kwargs): 
+        if self.request.user.is_authenticated:
+            instance = self.get_object()
+            if instance.author == self.request.user:
+                self.perform_destroy(instance)
+                return Response("OK", status=status.HTTP_200_OK)
+            else:
+                raise PermissionDenied("You are not the author of the post")
+                return Response("OK", status=status.HTTP_403_FORBIDDEN)
+        else:
+            raise NotAuthenticated("You are not authenticated")
+    
+class PostCommentViewSet(viewsets.ModelViewSet):
+    queryset = PostComment.objects.all()
+    serializer_class = PostCommentSerializer
+    
+    def perform_create(self, serializer):
+        if self.request.user.is_authenticated:
+            serializer.save(author=self.request.user)
+        else:
+            raise NotAuthenticated("You are not authenticated")
+
+
+    def update(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            raise NotAuthenticated("You are not authenticated")
+
+        instance = self.get_object()
+
+        if request.user != instance.post.author:
+            raise PermissionDenied("You are not the author of the post")
+            return Response("OK", status=status.HTTP_403_FORBIDDEN)
+
+        serializer = self.serializer_class(instance, data=request.data, partial = True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response("OK", status=status.HTTP_200_OK)
+
