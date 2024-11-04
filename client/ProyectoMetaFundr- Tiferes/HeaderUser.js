40dce8c5d41cb5459e61ed7196a3c20e1e275d3d
facueslar <facundoeslar@gmail.com>
2024-11-04 15:54:21 -0300

    Empezando perfil

--- client/ProyectoMetaFundr- Tiferes/HeaderUser.js
+++ client/ProyectoMetaFundr- Tiferes/HeaderUser.js
@@ -3,7 +3,27 @@ function goBack() {
 }
 
 document.getElementById('logout-button').addEventListener('click', function() {
-    localStorage.removeItem('access_token'); // Eliminar el token de acceso
-    console.log('Usuario deslogueado'); // Mensaje de confirmación
-    window.location.href = 'LogIn.html'; // Redirigir a la página de login
+    localStorage.removeItem('access_token'); 
+    console.log('Usuario deslogueado'); 
+    window.location.href = 'LogIn.html'; 
 });
+
+fetch(`http://localhost:8000/dj-rest-auth/user/`, {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+    "Content-Type": "application/json"
+  }
+})
+  .then((response) => response.json())
+  .then((user) => {
+    const usermail = user.email;
+    const name = user.first_name;
+    const Surname = user.last_name;
+    const dateofb = user.date_of_birth;
+    const number = user.phone_number;
+
+    console.log(user);
+
+  })
+  .catch((error) => console.error("Error:", error));
