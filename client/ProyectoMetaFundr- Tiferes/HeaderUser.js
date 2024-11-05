759de57c48e7d0e3fccc2bc2e1d40d11371b0bc4
facueslar <facundoeslar@gmail.com>
2024-11-05 14:23:33 -0300

    Perfil prpoio terminado

--- client/ProyectoMetaFundr- Tiferes/HeaderUser.js
+++ client/ProyectoMetaFundr- Tiferes/HeaderUser.js
@@ -1,29 +1,33 @@
 function goBack() {
-    window.location.href = 'main.html';
+  window.location.href = "main.html";
 }
 
-document.getElementById('logout-button').addEventListener('click', function() {
-    localStorage.removeItem('access_token'); 
-    console.log('Usuario deslogueado'); 
-    window.location.href = 'LogIn.html'; 
+document.getElementById("logout-button").addEventListener("click", function () {
+  localStorage.removeItem("access_token");
+  console.log("Usuario deslogueado");
+  window.location.href = "LogIn.html";
 });
 
 fetch(`http://localhost:8000/dj-rest-auth/user/`, {
   method: "GET",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 })
   .then((response) => response.json())
   .then((user) => {
-    const usermail = user.email;
     const name = user.first_name;
     const Surname = user.last_name;
-    const dateofb = user.date_of_birth;
     const number = user.phone_number;
+    const usern = user.username;
+    const dateofb = user.date_of_birth;
 
-    console.log(user);
-
+    document.getElementById("apellido").value = Surname;
+    document.getElementById("fecha-nacimiento").value = dateofb;
+    document.getElementById("Nombre").value = name;
+    document.getElementById("PhoneNumber").value = number;
+    document.getElementById("Nombre").value = name;
+    document.getElementById("userInput").value = usern;
   })
   .catch((error) => console.error("Error:", error));
